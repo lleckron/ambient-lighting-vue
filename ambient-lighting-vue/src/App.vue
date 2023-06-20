fad06b7d52e39c214592418b14409251fcd90edc
lleckron <leckronluke@gmail.com>
2023-06-19 20:03:25 -0400

    initial commit

---
+++ ambient-lighting-vue/src/App.vue
@@ -0,0 +1,151 @@
+<template>
+    <button class="toggle-button" @click="toggleDivs">Swap</button>
+
+    <div v-show="!videoShown" class="square" @click="switchImage()">
+        <img class="background-img" id="background-img" :src="imgSource" @load="getImageColors" alt="image" :style="shadowStyle"/>
+    </div>
+    
+    <div v-show="videoShown" class="square">
+        <video class="backrgroun-video" id="background-video" :src="vidSource" :style="shadowStyle" loop controls/>
+    </div>
+</template>
+
+<script>
+import banana from '@/assets/banana.webp'
+import watermelon from '@/assets/watermelon.webp'
+import wtfFruit from '@/assets/wtf-fruit-is-this.webp'
+import ambient480p from '@/assets/ambient-480p.mp4'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            videoShown: false,
+            imgSource: banana,
+			imgArray: [banana, watermelon, wtfFruit],
+            imgIndex: 0,
+			shadowStyle: String,
+            vidSource: ambient480p
+        }
+    },
+    methods: {
+        toggleDivs() {
+            this.videoShown = !this.videoShown
+        },
+
+        switchImage() {
+            this.imgIndex += 1
+            if(this.imgIndex > 2) {
+                this.imgIndex = 0
+            }
+            this.imgSource = this.imgArray[this.imgIndex]
+        },
+
+        getImageColors() {
+            const image = document.getElementById('background-img')
+            const canvas = document.createElement('canvas')
+            canvas.width = image.getBoundingClientRect().width
+            canvas.height = image.getBoundingClientRect().height
+
+            const context = canvas.getContext('2d')
+            context.drawImage(image, 0, 0, canvas.width, canvas.height)
+
+            const data = context.getImageData(0, 0, canvas.height, canvas.width)
+
+			this.findRGBAverage(data.data, canvas.width)
+        },
+
+		findRGBAverage(pixelData, width) {
+			const pixelRowLength = width * 4
+			let r = 0;
+			let g = 0;
+			let b = 0;
+			let denominator =  0;
+
+			for(let i = 0; i < pixelRowLength; i += 4) {
+				r += pixelData[i]
+				g += pixelData[i + 1]
+				b += pixelData[i + 2]
+				denominator += 1
+			}
+
+			const bottomRow = pixelData.length - pixelRowLength
+			for(let i = bottomRow; i < pixelData.length; i += 4) {
+				r += pixelData[i]
+				g += pixelData[i + 1]
+				b += pixelData[i + 2]
+				denominator += 1
+			}
+
+			this.setAmbientLight(r, g, b, denominator)
+		},
+
+		setAmbientLight(r, g, b, denominator) {
+			const avgR = Math.floor(r / denominator)
+			const avgG = Math.floor(g / denominator)
+			const avgB = Math.floor(b / denominator)
+
+			const shadowOne = '0 0 10px rgba(' + avgR + ', ' + avgG + ', ' + avgB + ', 0.2)'
+			const shadowTwo = '0 0 30px rgba(' + avgR + ', ' + avgG + ', ' + avgB + ', 0.4)'
+			const shadowThree= '0 0 50px rgba(' + avgR + ', ' + avgG + ', ' + avgB + ', 0.6)'
+			const shadowFour = '0 0 70px rgba(' + avgR + ', ' + avgG + ', ' + avgB + ', 0.8)'
+			const shadowFive = '0 0 100px rgba(' + avgR + ', ' + avgG + ', ' + avgB + ', 1)'
+
+			this.shadowStyle = 'box-shadow: ' + shadowOne + ', ' + shadowTwo + ', ' + shadowThree + ', ' + shadowFour + ', ' + shadowFive
+		}
+    }
+}
+</script>
+
+<style>
+html, body {
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+	background: #121212;
+}
+
+#app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+
+.square {
+    display: flex;
+    position: relative;
+    width: 75%;
+    height: 75%;
+}
+
+.square:hover {
+    cursor: pointer;
+}
+
+.background-img {
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+}
+
+.toggle-button {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: transparent;
+    border: 2px solid #e9e9e9;
+    border-radius: 6px;
+    font-size: 25px;
+    color: #e9e9e9;
+    margin: 30px 0 0 30px;
+}
+
+.toggle-button:hover {
+    cursor: pointer;
+    color: #c9c9c9;
+    border-color: #c9c9c9;
+}
+</style>
